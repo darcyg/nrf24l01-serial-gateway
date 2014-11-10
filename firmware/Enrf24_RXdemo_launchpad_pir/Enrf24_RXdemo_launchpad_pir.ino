c727c8d15c5c025ff76ea6379d8866b2fa8d8f77
frankalicious <af.frankalicious@googlemail.com>
2014-11-10 20:58:26 +0100

    add status output in the beginning
    In the end of the setup function outpu the status of all registers to the serial port. The not available registers in between are omitted.

--- firmware/Enrf24_RXdemo_launchpad_pir/Enrf24_RXdemo_launchpad_pir.ino
+++ firmware/Enrf24_RXdemo_launchpad_pir/Enrf24_RXdemo_launchpad_pir.ino
@@ -13,6 +13,39 @@ const char *str_off = "OFF";
 
 void dump_radio_status_to_serialport(uint8_t);
 
+char* cmd_name[]={
+  "CONFIG",
+  "EN_AA",
+  "EN_RXADDR",
+  "SETUP_AW",
+  "SETUP_RETR",
+  "RF_CH",
+  "RF_SETUP",
+  "STATUS",
+  "OBSERVE_TX",
+  "CD",
+  "RX_ADDR_P0",
+  "RX_ADDR_P1",
+  "RX_ADDR_P2",
+  "RX_ADDR_P3",
+  "RX_ADDR_P4",
+  "RX_ADDR_P5",
+  "TX_ADDR",
+  "RX_PW_P0",
+  "RX_PW_P1",
+  "RX_PW_P2",
+  "RX_PW_P3",
+  "RX_PW_P4",
+  "RX_PW_P5",
+  "FIFO_STATUS",
+  "N/A",
+  "N/A",
+  "N/A",
+  "N/A",
+  "DYNPD",
+  "FEATURE"
+};
+
 void setup() {
   Serial.begin(9600);
 
@@ -31,6 +64,8 @@ void setup() {
    digitalWrite(P1_0, LOW);
    */
   radio.enableRX();  // Start listening
+
+  register_status();
 }
 
 void loop() {
@@ -78,6 +113,65 @@ void dump_radio_status_to_serialport(uint8_t status)
 
   default:
     Serial.println("UNKNOWN STATUS CODE");
+  }
+}
+
+void register_status(void)
+{
+  uint8_t reg;
+
+
+  for (reg = 0; reg <=0x1d ; reg++)
+  {
+
+    if (reg == 0xa || reg == 0xb || reg == 0x10)
+    {
+      uint8_t value[5];
+      radio._readRegMultiLSB(reg, &value[0],5);
+      //value = radio.readReg(reg);
+      //Serial.print("register:0x");
+      //if (reg<0x10)
+      //  Serial.print("0");
+      //Serial.print(reg, HEX);
+      //Serial.print(" value:");
+      Serial.print("\"");
+      Serial.print(cmd_name[reg]);
+      Serial.print("\" ");
+      uint8_t i;
+      for (i=0;i<5;i++)
+      {
+        Serial.print("0x");
+        if (value[i]<0x10)
+          Serial.print("0");
+        Serial.print(value[i],HEX);
+        //add space between values if not last value. last value needs a newline
+        if (i < 4)
+        {
+          Serial.print(" ");
+        }
+        else
+        {
+          Serial.println("");
+        }
+      }
+    }
+    else
+    {
+      uint8_t value;
+      value = radio.readReg(reg);
+      //Serial.print("register:0x");
+      //if (reg<0x10)
+      //  Serial.print("0");
+      //Serial.print(reg, HEX);
+      //Serial.print(" value:0x");
+      Serial.print("\"");
+      Serial.print(cmd_name[reg]);
+      Serial.print("\"");
+      Serial.print(" 0x");
+      if (value<0x10)
+        Serial.print("0");
+      Serial.println(value,HEX);
+    }
   }
 }
 
